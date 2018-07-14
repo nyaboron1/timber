ef5a47abae83f7edb3e32973e97e5b2e73a6ed9a
nyaboron1 <den.alihyder@protonmail.com>
2018-07-14 18:19:47 +0200

    beeeeeeemo :D

--- Timber.cpp
+++ Timber.cpp
@@ -37,7 +37,8 @@ int main()
   bool beeActive = false;
   float beeSpeed = 0.0f;
   
-
+  Clock clock;
+  
   while (window.isOpen())
     {
       /*******************************
@@ -51,8 +52,38 @@ int main()
       /*******************************
 	Update the scene
       *******************************/
+      Time dt = clock.restart(); // delta time
 
+      // update bee
+      if (!beeActive)
+	{
+	  // how fast is the bee
+	  srand ((int)time(0));
+	  beeSpeed = (rand() % 200) + 200;
 
+	  // how high is the bee
+	  srand ((int)time(0) * 10);
+	  float height = (rand() % 500) + 500;
+	  sBee.setPosition (200, height);
+	  beeActive = true;
+	}
+      // move the bee
+      else
+	{
+	  sBee.setPosition
+	    (sBee.getPosition().x -
+	     (beeSpeed * dt.asSeconds()),
+	     sBee.getPosition().y
+	     );
+	  // bee reached the right hand
+	  if (sBee.getPosition().x < -100)
+	    {
+	      // set ready for update
+	      beeActive = false;
+	    }
+
+	}
+      
       /*******************************
 	Draw the scene
       *******************************/
