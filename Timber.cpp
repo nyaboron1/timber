ad13a9e348d1e7cbb82eea463d02e39ef062d970
nyaboron1 <den.alihyder@protonmail.com>
2018-07-15 18:17:06 +0200

    score and pause

--- Timber.cpp
+++ Timber.cpp
@@ -1,4 +1,7 @@
 #include <SFML/Graphics.hpp>
+
+#include <sstream>
+
 using namespace sf;
 
 int main()
@@ -36,6 +39,28 @@ int main()
   Sprite sCloud4;
   sCloud4.setTexture(tCloud);
 
+  Font font;
+  font.loadFromFile("resources/fonts/KOMIKAP_.ttf");
+
+  Text messageText;
+  messageText.setFont(font);
+  messageText.setString("Press Enter to start");
+  messageText.setCharacterSize(75);
+  messageText.setFillColor(Color::White);
+  messageText.setPosition(1920/2, 1080/2);
+  Text scoreText;
+  scoreText.setFont(font);
+  scoreText.setString("Score = 0");
+  scoreText.setCharacterSize(100);
+  scoreText.setFillColor(Color::White);
+  scoreText.setPosition(20, 20);
+
+  FloatRect textRect = messageText.getLocalBounds();
+  messageText.setOrigin(textRect.left +
+			 textRect.width / 2.0f,
+			 textRect.top +
+			 textRect.height / 2.0f);
+  
   /****************************
     Variables
   *****************************/
@@ -44,17 +69,21 @@ int main()
   bool cloud2Active = false;
   bool cloud3Active = false;
   bool cloud4Active = false;
+  bool pause = true; // game running
   
   float beeSpeed = 0.0f;
   float cloud1Speed = 0.0f;
   float cloud2Speed = 0.0f;
   float cloud3Speed = 0.0f;
   float cloud4Speed = 0.0f;
+
+  int score = 0; 
   
   Clock clock;
   
   while (window.isOpen())
     {
+      score++;
       /*******************************
 	Handle the players input
       ********************************/
@@ -63,118 +92,132 @@ int main()
 	  window.close();
 	}
 
+      // start he game
+      if (Keyboard::isKeyPressed(Keyboard::Return))
+	{
+	  pause = false;
+	}
+
       /*******************************
 	Update the scene
       *******************************/
-      Time dt = clock.restart(); // delta time
-
-      // update bee
-      if (!beeActive)
+      if (!pause)
 	{
-	  // how fast is the bee
-	  srand ((int)time(0));
-	  beeSpeed = (rand() % 200) + 200;
+	  Time dt = clock.restart(); // delta time
 
-	  // how high is the bee
-	  srand ((int)time(0) * 10);
-	  float height = (rand() % 500) + 500;
-	  float high = (rand() % 1920);
-	  sBee.setPosition (high, height);
-	  beeActive = true;
-	}
-      // move the bee
-      else
-	{
-	  sBee.setPosition
-	    (sBee.getPosition().x -
-	     (beeSpeed * dt.asSeconds()),
-	     sBee.getPosition().y
-	     );
-	  // bee reached the right hand
-	  if (sBee.getPosition().x < -100)
+	  // update bee
+	  if (!beeActive)
 	    {
-	      // set ready for update
-	      beeActive = false;
-	    }
+	      // how fast is the bee
+	      srand ((int)time(0));
+	      beeSpeed = (rand() % 200) + 200;
 
-	}
+	      // how high is the bee
+	      srand ((int)time(0) * 10);
+	      float height = (rand() % 500) + 500;
+	      float high = (rand() % 1920);
+	      sBee.setPosition (high, height);
+	      beeActive = true;
+	    }
+	  // move the bee
+	  else
+	    {
+	      sBee.setPosition
+		(sBee.getPosition().x -
+		 (beeSpeed * dt.asSeconds()),
+		 sBee.getPosition().y
+		 );
+	      // bee reached the right hand
+	      if (sBee.getPosition().x < -100)
+		{
+		  // set ready for update
+		  beeActive = false;
+		}
+
+	    }
 
       
-      if (!cloud1Active)
-	{
-	  // how fast is the cloud
-	  srand((int)time(0) * 10);
-	  cloud1Speed = (rand() % 200);
-
-	  // how high is the cloud
-	  srand((int)time(0) * 10);
-	  float height = (rand() % 150);
-	  sCloud1.setPosition(-200, height);
-	  cloud1Active = true;
-	}
-      else
-	{
-	  sCloud1.setPosition
-	    (sCloud1.getPosition().x +
-	     (cloud1Speed * dt.asSeconds()),
-	     sCloud1.getPosition().y);
-
-	  if (sCloud1.getPosition().x > 1920)
+	  if (!cloud1Active)
 	    {
-	      cloud1Active = false;
+	      // how fast is the cloud
+	      srand((int)time(0) * 10);
+	      cloud1Speed = (rand() % 200);
+
+	      // how high is the cloud
+	      srand((int)time(0) * 10);
+	      float height = (rand() % 150);
+	      sCloud1.setPosition(-200, height);
+	      cloud1Active = true;
 	    }
-	}
+	  else
+	    {
+	      sCloud1.setPosition
+		(sCloud1.getPosition().x +
+		 (cloud1Speed * dt.asSeconds()),
+		 sCloud1.getPosition().y);
+
+	      if (sCloud1.getPosition().x > 1920)
+		{
+		  cloud1Active = false;
+		}
+	    }
 
       
-      if (!cloud2Active)
-	{
-	  // how fast is the cloud
-	  srand((int)time(0) * 20);
-	  cloud2Speed = (rand() % 200);
-
-	  // how high is the cloud
-	  srand((int)time(0) * 20);
-	  float height = (rand() % 300) - 150;
-	  sCloud2.setPosition(-200, height);
-	  cloud2Active = true;
-	}
-      else
-	{
-	  sCloud2.setPosition
-	    (sCloud2.getPosition().x +
-	     (cloud2Speed * dt.asSeconds()),
-	     sCloud2.getPosition().y);
-
-	  if (sCloud2.getPosition().x > 1920)
+	  if (!cloud2Active)
 	    {
-	      cloud2Active = false;
+	      // how fast is the cloud
+	      srand((int)time(0) * 20);
+	      cloud2Speed = (rand() % 200);
+
+	      // how high is the cloud
+	      srand((int)time(0) * 20);
+	      float height = (rand() % 300) - 150;
+	      sCloud2.setPosition(-200, height);
+	      cloud2Active = true;
+	    }
+	  else
+	    {
+	      sCloud2.setPosition
+		(sCloud2.getPosition().x +
+		 (cloud2Speed * dt.asSeconds()),
+		 sCloud2.getPosition().y);
+
+	      if (sCloud2.getPosition().x > 1920)
+		{
+		  cloud2Active = false;
+		}
+	    }
+
+	  if (!cloud3Active)
+	    {
+	      // how fast is the cloud
+	      srand((int)time(0) * 10);
+	      cloud3Speed = (rand() % 300);
+
+	      // how high is the cloud
+	      srand((int)time(0) * 10);
+	      float height = (rand() % 450) - 150;
+	      sCloud3.setPosition(-200, height);
+	      cloud3Active = true;
+	    }
+	  else
+	    {
+	      sCloud3.setPosition
+		(sCloud3.getPosition().x +
+		 (cloud3Speed * dt.asSeconds()),
+		 sCloud3.getPosition().y);
+
+	      if (sCloud3.getPosition().x > 1920)
+		{
+		  cloud3Active = false;
+		}
 	    }
 	}
 
-      if (!cloud3Active)
-	{
-	  // how fast is the cloud
-	  srand((int)time(0) * 10);
-	  cloud3Speed = (rand() % 300);
-
-	  // how high is the cloud
-	  srand((int)time(0) * 10);
-	  float height = (rand() % 450) - 150;
-	  sCloud3.setPosition(-200, height);
-	  cloud3Active = true;
-	}
-      else
-	{
-	  sCloud3.setPosition
-	    (sCloud3.getPosition().x +
-	     (cloud3Speed * dt.asSeconds()),
-	     sCloud3.getPosition().y);
-
-	  if (sCloud3.getPosition().x > 1920)
-	    {
-	      cloud3Active = false;
-	    }
-	}
+      // Update the score text
+      std::stringstream ss;
+      ss << "Score = " << score;
+      scoreText.setString(ss.str());
 
       
       /*******************************
@@ -188,6 +231,9 @@ int main()
       window.draw(sCloud1);
       window.draw(sCloud2);
       window.draw(sCloud3);
+      window.draw(scoreText);
+      if (pause)
+	  window.draw(messageText);
       
       window.display();
     }
