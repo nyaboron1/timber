fabd62e643bcc68fbb2346a5a00db82dac5f45a9
nyaboron1 <den.alihyder@protonmail.com>
2018-07-19 23:17:16 +0200

    animating chopped logs and axe

--- Timber.cpp
+++ Timber.cpp
@@ -189,6 +189,21 @@ int main()
       /*******************************
 	Handle the players input
       ********************************/
+
+      Event event;
+      while (window.pollEvent(event))
+	{
+	  if (event.type == Event::KeyReleased && !pause)
+	    {
+	      // Listen for key presses again
+	      acceptInput = true;
+
+	      // hide the axe
+	      sAxe.setPosition(2000, sAxe.getPosition().y);
+	    }
+	}
+
+      
       if (Keyboard::isKeyPressed(Keyboard::Escape))
 	{
 	  window.close();
@@ -222,7 +237,7 @@ int main()
       if (acceptInput)
 	{
 	  // Handle pressing the right cursor key
-	  if (KeyBoard::isKeyPressed(Keyboard::Right))
+	  if (Keyboard::isKeyPressed(Keyboard::Right))
 	    {
 	      playerSide = side::RIGHT;
 	      score++;
@@ -230,7 +245,7 @@ int main()
 	      // Add to the amount of time remaining
 	      timeRemaining += (2 / score) + 0.15f;
 
-	      sAxe.setPosition(AXE_POSITION_RIGHT,
+	      sAxe.setPosition(axePositionRight,
 			       sAxe.getPosition().y);
 
 	      sPlayer.setPosition(1200,720);
@@ -245,7 +260,7 @@ int main()
 
 	      acceptInput = false;
 	    }
-	  else if (KeyBoard::isKeyPressed(Keyboard::Left))
+	  else if (Keyboard::isKeyPressed(Keyboard::Left))
 	    { 
 	      playerSide = side::LEFT;
 	      score++;
@@ -253,7 +268,7 @@ int main()
 	      // Add to the amount of time remaining
 	      timeRemaining += (2 / score) + 0.15f;
 
-	      sAxe.setPosition(AXE_POSITION_LEFT,
+	      sAxe.setPosition(axePositionLeft,
 			       sAxe.getPosition().y);
 
 	      sPlayer.setPosition(580,720);
@@ -329,7 +344,6 @@ int main()
 		  // set ready for update
 		  beeActive = false;
 		}
-
 	    }
 
       
@@ -408,34 +422,53 @@ int main()
 		  cloud3Active = false;
 		}
 	    }
-	}
-      
-      // Update the score text
-      std::stringstream ss;
-      ss << "Score = " << score;
-      scoreText.setString(ss.str());
 
-      // Update the branch sprites
-      for (int i = 0; i < NUM_BRANCHES; ++i)
-	{
-	  float height = i * 150;
-	  if (branchPositions[i] == side::LEFT)
+	  // Update the score text
+	  std::stringstream ss;
+	  ss << "Score = " << score;
+	  scoreText.setString(ss.str());
+      
+	  // Update the branch sprites
+	  for (int i = 0; i < NUM_BRANCHES; ++i)
 	    {
-	      // move the sprite to the left side
-	      branches[i].setPosition(610,height);
-	      // Flip the msprite round the other way
-	      branches[i].setRotation(180);
+	      float height = i * 150;
+	      if (branchPositions[i] == side::LEFT)
+		{
+		  // move the sprite to the left side
+		  branches[i].setPosition(610,height);
+		  // Flip the msprite round the other way
+		  branches[i].setRotation(180);
+		}
+	      else if (branchPositions[i] == side::RIGHT)
+		{
+		  branches[i].setPosition(1330,height);
+		  branches[i].setRotation(0);
+		}
+	      else
+		{
+		  // Hide the branch
+		  branches[i].setPosition(3000, height);
+		}
+
+	      // Handle a flying log
+	      if (logActive)
+		{
+		  sLog.setPosition(sLog.getPosition().x +
+				   (logSpeedX * dt.asSeconds()),
+				   sLog.getPosition().y +
+				   (logSpeedY * dt.asSeconds()));
+
+		  // Has the log reached the right hand edge?
+		  if (sLog.getPosition().x < -100 ||
+		      sLog.getPosition().x > 2000)
+		    {
+		      // set it up ready
+		      logActive = false;
+		      sLog.setPosition(810, 720);
+		    }
+		}
 	    }
-	  else if (branchPositions[i] == side::RIGHT)
-	    {
-	      branches[i].setPosition(1330,height);
-	      branches[i].setRotation(0);
-	    }
-	  else
-	    {
-	      // Hide the branch
-	      branches[i].setPosition(3000, height);
-	    }
+	  
 	}
       
       /*******************************
@@ -448,9 +481,10 @@ int main()
       window.draw(sCloud3);
       window.draw(sTree);
 
-      for (int i = 0; i < NUM_BRANCHES; i++) {
-	window.draw(branches[i]);
-      }
+      for (int i = 0; i < NUM_BRANCHES; i++)
+	{
+	  window.draw(branches[i]);
+	}
       
       window.draw(sPlayer);
       window.draw(sAxe);
